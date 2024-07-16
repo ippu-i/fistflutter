90b6ef3db547c2ba85f97767b278f34ee10767a0
ippu-i <162537780+ippu-i@users.noreply.github.com>
2024-07-16 15:45:59 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,14 +5,14 @@ set -euxo pipefail
 cd /workspaces/
 git clone https://github.com/flutter/flutter.git -b stable
 
-sudo apt-get update
-sudo apt-get install clang build-essential cmake ninja-build pkg-config gtk-3.0
+#sudo apt-get update
+#sudo apt-get install clang build-essential cmake ninja-build pkg-config gtk-3.0
 
 cd /workspaces/fistflutter/first_flutter/
 
 flutter --version
-flutter pub global activate melos
-flutter pub global activate grinder
+#flutter pub global activate melos
+#flutter pub global activate grinder
 flutter pub get --no-example
 dart pub get --no-example
 
